2761e2ec85bc58b1c769445e32635d2a18c212d5
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-27 22:21:26 -0400

    create a class

---
+++ src/demo/repository2/DeoRepoTest2.java
@@ -0,0 +1,9 @@
+package demo.repository2;
+
+public class DeoRepoTest2 {
+	
+	public static void main(String[] args) {
+		System.out.println("This is a demo to create a repository");
+		System.out.println("demo");
+	}
+}
